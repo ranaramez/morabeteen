8e39c6df55c1b94ab0b84197ee4b0cbaf4510b0f
Kareem Ali <kerie.fcis@gmail.com>
2013-06-22 14:46:29 +0200

    initializing ui environment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require bootstrap-select
+//= require bootstrap-datepicker
 //= require toastr
 //= require tasks
 //= require users
+
+
+$(function(){
+  $('select').selectpicker();
+});
